74347ed6d6274197f7a658dddf11fe0333c34bba
josephlesterbacs <106429121+jlbbacs@users.noreply.github.com>
2026-03-09 14:40:53 +0800

    fixing sanityClient

--- sanityclient.js
+++ sanityclient.js
@@ -2,10 +2,11 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.VITE_SANITY_DATASET,
+  projectId: 'pykxlxp8',
+  dataset: 'production',
   useCdn: true,
   apiVersion: '2024-01-01',
+  perspective: 'published',
 })
 
 const builder = imageUrlBuilder(client)
